53587c865baf08199a65d1fda71c4974dc1e13ef
Norbert Renner <norbert.renner@gmx.de>
2014-05-14 13:14:35 +0200

    crosshair cursor instead of marker

--- js/plugin/Routing.js
+++ js/plugin/Routing.js
@@ -1,16 +1,25 @@
 BR.Routing = L.Routing.extend({
     options: {
-        /* not implemented yet
         icons: {
+            /* not implemented yet
             start: new L.Icon.Default({iconUrl: 'bower_components/leaflet-gpx/pin-icon-start.png'}),
             end: new L.Icon.Default(),
             normal: new L.Icon.Default()
-        },*/
+            */
+            draw: false
+        },
         snapping: null
     },
 
     onAdd: function (map) {
         var container = L.Routing.prototype.onAdd.call(this, map);
+
+        this._draw.on('enabled', function() {
+            L.DomUtil.addClass(map.getContainer(), 'routing-draw-enabled');
+        });
+        this._draw.on('disabled', function() {
+            L.DomUtil.removeClass(map.getContainer(), 'routing-draw-enabled');
+        });
 
         // enable drawing mode
         this.draw(true);
