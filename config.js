57414cff59970f31cab82e137d7e749ea46adc0f
Norbert Renner <norbert.renner@gmx.de>
2014-05-30 12:26:34 +0200

    profile config for online service (brouter.de)

--- config.js
+++ config.js
@@ -1,13 +1,44 @@
-BR.conf = {
-    profiles: [
-        'trekking',
-        'fastbike',
-        'shortest',
-        'all',
-        /* "does not contain expressions for context node" (?)
-        'softaccess',
-        */
-        'moped',
-        'car-test'
-    ]
-};
+(function() {
+
+    var hostname = window.location.hostname;
+
+    BR.conf = {};
+
+    if (hostname === 'brouter.de' || hostname === 'h2096617.stratoserver.net') {
+
+        // online service (brouter.de) configuration
+
+        BR.conf.profiles = [
+            'trekking',
+            'fastbike',
+            'shortest',
+            'shortest-eudem',
+            'all',
+            /* "does not contain expressions for context node" (?)
+             'softaccess',
+             */
+            'moped',
+            'car-test',
+            'vm-forum-liegerad-schnell',
+            'vm-forum-velomobil-schnell'
+        ];
+
+    } else {
+
+        // desktop configuration
+
+        BR.conf.profiles = [
+            'trekking',
+            'fastbike',
+            'shortest',
+            'all',
+            /* "does not contain expressions for context node" (?)
+             'softaccess',
+             */
+            'moped',
+            'car-test'
+        ];
+
+    }
+
+})();
