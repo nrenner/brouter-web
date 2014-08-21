d260414f6ce39b181cd068ccae8d95556cc353d2
Norbert Renner <norbert.renner@gmx.de>
2014-08-21 15:22:21 +0200

    remove unnecessary target="_blank" for download links

--- js/control/Download.js
+++ js/control/Download.js
@@ -11,12 +11,12 @@ BR.Download = BR.Control.extend({
     update: function (urls) {
         var html = '<div class="value">';
         if (urls.gpx) {
-            html += '<a href="' + urls.gpx + '" download="brouter.gpx" target="_blank">GPX</a> &middot; ';
-            html += '<a href="' + urls.kml + '" download="brouter.kml" target="_blank">KML</a> &middot; ';
+            html += '<a href="' + urls.gpx + '" download="brouter.gpx">GPX</a> &middot; ';
+            html += '<a href="' + urls.kml + '" download="brouter.kml">KML</a> &middot; ';
             html += '<a href="' + urls.geojson + '" download="brouter.geojson">GeoJSON</a> &middot; ';
-            html += '<a href="' + urls.csv + '" download="brouter.tsv" target="_blank">CSV</a>';
+            html += '<a href="' + urls.csv + '" download="brouter.tsv">CSV</a>';
         }
-        html += '</div>'
+        html += '</div>';
         this._content.innerHTML = html;
     }
 });
