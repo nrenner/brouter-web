a54cc31a0ffa4c4d82250239cd1883e161927160
Norbert Renner <norbert.renner@gmx.de>
2016-06-08 18:19:54 +0200

    Add mean cost to stats (+ small refactoring), closes #39

--- js/control/TrackStats.js
+++ js/control/TrackStats.js
@@ -11,14 +11,17 @@ BR.TrackStats = BR.Control.extend({
 
     update: function (polyline, segments) {
         var stats = this.calcStats(polyline, segments),
+            length1 = L.Util.formatNum(stats.trackLength/1000,1),
+            length3 = L.Util.formatNum(stats.trackLength/1000,3),
+            meanCostFactor = stats.trackLength ? L.Util.formatNum(stats.cost / stats.trackLength, 2) : '',
             html = '';
 
         html += '<table id="stats">';
-        html += '<tr><td>Length: </td><td title="' + L.Util.formatNum(stats.trackLength/1000,3) + ' km">'
-                + L.Util.formatNum(stats.trackLength/1000,1) + '</td><td>km</td></tr>';
+        html += '<tr><td>Length: </td><td title="' + length3 + ' km">' + length1 + '</td><td>km</td></tr>';
         html += '<tr><td>Ascent filtered:</td><td>' + stats.filteredAscend + '</td><td>m</td></tr>';
         html += '<tr><td>Ascent plain:</td><td>' + stats.plainAscend + '</td><td>m</td></tr>';
         html += '<tr><td>Cost: </td><td>' + stats.cost + '</td><td></td></tr>';
+        html += '<tr><td>Mean cost:</td><td>' + meanCostFactor + '</td><td></td></tr>';
         html += '</table>'; 
 
         this._content.innerHTML = html;
