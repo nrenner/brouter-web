86f55e00d2b06e162c6a063272fbbac676d70f98
Norbert Renner <norbert.renner@gmx.de>
2019-04-12 20:09:51 +0200

    Add map URL for all layers

--- js/LayersConfig.js
+++ js/LayersConfig.js
@@ -88,11 +88,6 @@ BR.LayersConfig = L.Class.extend({
                 'name': i18next.t('map.layer.osm'),
                 'mapUrl': 'https://www.openstreetmap.org/#map={zoom}/{lat}/{lon}'
             },
-            'osm-mapnik-german_style': {
-                'name': i18next.t('map.layer.osmde'),
-                'language_code': 'de',
-                'mapUrl': 'https://www.openstreetmap.de/karte.html?zoom={zoom}&lat={lat}&lon={lon}&layers=B000TF'
-            },
             'OpenTopoMap': {
                 'name': i18next.t('map.layer.topo'),
                 'mapUrl': 'https://opentopomap.org/#map={zoom}/{lat}/{lon}'
@@ -100,6 +95,22 @@ BR.LayersConfig = L.Class.extend({
             'Stamen.Terrain': {
                 'name': i18next.t('map.layer.stamen-terrain'),
                 'mapUrl': 'http://maps.stamen.com/#terrain/{zoom}/{lat}/{lon}'
+            },
+            'Esri.WorldImagery': {
+                'name': i18next.t('map.layer.esri'),
+                'nameShort': i18next.t('credits.esri-tiles'),
+                'mapUrl': 'http://www.arcgis.com/home/item.html?id=10df2279f9684e4a9f6a7f08febac2a9'
+            },
+            'wikimedia-map': {
+                'mapUrl': 'https://maps.wikimedia.org/#{zoom}/{lat}/{lon}'
+            },
+            'HDM_HOT': {
+                'nameShort': 'HOT',
+                'mapUrl': 'http://map.hotosm.org/#{zoom}/{lat}/{lon}'
+            },
+            // OpenStreetMap.se (Hydda.Full)
+            '1010': {
+                'mapUrl': 'https://maps.openstreetmap.se/#{zoom}/{lat}/{lon}'
             },
             'opencylemap': {
                 'name': i18next.t('map.layer.cycle'),
@@ -111,10 +122,112 @@ BR.LayersConfig = L.Class.extend({
                 'nameShort': 'Outdoors',
                 'mapUrl': 'https://www.opencyclemap.org/?zoom={zoom}&lat={lat}&lon={lon}&layers=000B0'
             },
-            'Esri.WorldImagery': {
-                'name': i18next.t('map.layer.esri'),
-                'nameShort': i18next.t('credits.esri-tiles'),
-                'mapUrl': 'http://www.arcgis.com/home/item.html?id=10df2279f9684e4a9f6a7f08febac2a9'
+            // Hike & Bike Map
+            '1065': {
+                'mapUrl': 'http://hikebikemap.org/?zoom={zoom}&lat={lat}&lon={lon}&layer=HikeBikeMap'
+            },
+            // 4UMaps
+            '1016': {
+                'mapUrl': 'https://www.4umaps.com/map.htm?zoom={zoom}&lat={lat}&lon={lon}&layers=B00'
+            },
+            'openmapsurfer': {
+                'mapUrl': 'https://maps.openrouteservice.org/directions?n1={lat}&n2={lon}&n3={zoom}&b=0&c=0&k1=en-US&k2=km'
+            },
+            'osm-mapnik-german_style': {
+                'name': i18next.t('map.layer.osmde'),
+                'language_code': 'de',
+                'mapUrl': 'https://www.openstreetmap.de/karte.html?zoom={zoom}&lat={lat}&lon={lon}&layers=B000TF'
+            },
+            'osmfr': {
+                'language_code': 'fr',
+                'nameShort': 'OSM French',
+                'mapUrl': 'http://tile.openstreetmap.fr/?layers=B00000000FFFFFF&zoom={zoom}&lat={lat}&lon={lon}'
+            },
+            // Osmapa.pl - Mapa OpenStreetMap Polska
+            '1017': {
+                'language_code': 'pl',
+                'mapUrl': 'http://osmapa.pl/#lat={lat}&lon={lon}&z={zoom}&m=os'
+            },
+            // kosmosnimki.ru
+            '1023': {
+                'language_code': 'ru',
+                'mapUrl': 'http://kosmosnimki.ru/'
+            },
+            // sputnik.ru
+            '1021': {
+                'language_code': 'ru',
+                'mapUrl': 'http://maps.sputnik.ru/?lat={lat}&lng={lon}&zoom={zoom}'
+            },
+            'MtbMap': {
+                'mapUrl': 'http://mtbmap.cz/#zoom={zoom}&lat={lat}&lon={lon}'
+            },
+            // MRI (maps.refuges.info)
+            '1069': {
+                'nameShort': 'Refuges.info',
+                'mapUrl': 'http://maps.refuges.info/?zoom={zoom}&lat={lat}&lon={lon}&layers=B'
+            },
+            'osmfr-basque': {
+                'language_code': 'eu',
+                'nameShort': 'OSM Basque',
+                'mapUrl': 'http://tile.openstreetmap.fr/?layers=00000000BFFFFFF&zoom={zoom}&lat={lat}&lon={lon}'
+            },
+            'osmfr-breton': {
+                'language_code': 'br',
+                'nameShort': 'OSM Breton',
+                'mapUrl': 'https://kartenn.openstreetmap.bzh/#map={zoom}/{lat}/{lon}'
+            },
+            'osmfr-occitan': {
+                'language_code': 'oc',
+                'nameShort': 'OSM Occitan',
+                'mapUrl': 'http://tile.openstreetmap.fr/?layers=0000000B0FFFFFF&zoom={zoom}&lat={lat}&lon={lon}'
+            },
+            'osmbe': {
+                'nameShort': 'OSM Belgium',
+                'mapUrl': 'https://tile.osm.be/#map={zoom}/{lat}/{lon}'
+            },
+            'osmbe-fr': {
+                'nameShort': 'OSM Belgium (fr)',
+                'mapUrl': 'https://tile.osm.be/#map={zoom}/{lat}/{lon}'
+            },
+            'osmbe-nl': {
+                'nameShort': 'OSM Belgium (nl)',
+                'mapUrl': 'https://tile.osm.be/#map={zoom}/{lat}/{lon}'
+            },
+            'OpenStreetMap.CH': {
+                'country_code': 'CH',
+                'mapUrl': 'https://osm.ch/#{zoom}/{lat}/{lon}'
+            },
+            'topplus-open': {
+                'country_code': 'DE',
+                'mapUrl': 'http://www.geodatenzentrum.de/geodaten/gdz_rahmen.gdz_div?gdz_spr=deu&gdz_user_id=0&gdz_akt_zeile=5&gdz_anz_zeile=1&gdz_unt_zeile=41'
+            },
+
+            'OpenStreetMap-turistautak': {
+                'nameShort': 'OSM Turistautak',
+                'mapUrl': 'https://turistautak.openstreetmap.hu/?zoom={zoom}&lat={lat}&lon={lon}&layers=0B00F'
+            },
+            'Israel_Hiking': {
+                'mapUrl': 'https://israelhiking.osm.org.il/map/{zoom}/{lat}/{lon}'
+            },
+            'Israel_MTB': {
+                'mapUrl': 'https://israelhiking.osm.org.il/map/{zoom}/{lat}/{lon}'
+            },
+            'mtbmap-no': {
+                'mapUrl': 'https://mtbmap.no/#{zoom}/{lat}/{lon}'
+            },
+            'Freemap.sk-Car': {
+                'mapUrl': 'https://www.freemap.sk/?map={zoom}/{lat}/{lon}&layers=A'
+            },
+            'Freemap.sk-Hiking': {
+                'mapUrl': 'https://www.freemap.sk/?map={zoom}/{lat}/{lon}&layers=T'
+            },
+            'Freemap.sk-Cyclo': {
+                'mapUrl': 'https://www.freemap.sk/?map={zoom}/{lat}/{lon}&layers=C'
+            },
+            'osm-cambodia_laos_thailand_vietnam-bilingual': {
+                'country_code': 'TH+',
+                'nameShort': 'Thaimap',
+                'mapUrl': 'http://thaimap.osm-tools.org/?zoom={zoom}&lat={lat}&lon={lon}&layers=BT'
             },
             'HikeBike.HillShading': {
                 'name': i18next.t('map.layer.hikebike-hillshading'),
@@ -131,38 +244,40 @@ BR.LayersConfig = L.Class.extend({
                 'nameShort': i18next.t('map.hiking'),
                 'mapUrl': 'http://hiking.waymarkedtrails.org/#?map={zoom}!{lat}!{lon}'
             },
-            'OpenStreetMap.CH': {
-                'country_code': 'CH'
+            'Waymarked_Trails-MTB': {
+                'nameShort': 'MTB',
+                'mapUrl': 'http://mtb.waymarkedtrails.org/#?map={zoom}!{lat}!{lon}'
             },
-            'topplus-open': {
-                'country_code': 'DE'
+            'mapillary-coverage-raster': {
+                'nameShort': 'Mapillary',
+                'mapUrl': 'https://www.mapillary.com/app/?lat={lat}&lng={lon}&z={zoom}&menu=false'
             },
-            'osm-cambodia_laos_thailand_vietnam-bilingual': {
-                'country_code': 'TH+'
+            'historic-place-contours': {
+                'mapUrl': 'http://gk.historic.place/historische_objekte/?zoom={zoom}&lat={lat}&lon={lon}&pid=GhHaSaHe'
             },
-            'osmfr': {
-                'language_code': 'fr'
+            'hu-hillshade': {
+                'nameShort': 'Hillshade HU',
+                'mapUrl': 'http://map.turistautak.hu/?zoom={zoom}&lat={lat}&lon={lon}&layers=0B000FTF'
             },
-            // kosmosnimki.ru
-            '1023': {
-                'language_code': 'ru'
+            'mapaszlakow-cycle': {
+                'nameShort': 'Cycleways PL',
+                'mapUrl': 'http://mapaszlakow.eu/#{zoom}/{lat}/{lon}'
             },
-            // sputnik.ru
-            '1021': {
-                'language_code': 'ru'
+            'mapaszlakow-bike': {
+                'nameShort': 'Bicycle PL',
+                'mapUrl': 'http://mapaszlakow.eu/#{zoom}/{lat}/{lon}'
             },
-            // Osmapa.pl - Mapa OpenStreetMap Polska
-            '1017': {
-                'language_code': 'pl'
+            'mapaszlakow-hike': {
+                'nameShort': 'Hiking PL',
+                'mapUrl': 'http://mapaszlakow.eu/#{zoom}/{lat}/{lon}'
             },
-            'osmfr-basque': {
-                'language_code': 'eu'
+            'mapaszlakow-mtb': {
+                'nameShort': 'MTB:scale PL',
+                'mapUrl': 'http://mapaszlakow.eu/#{zoom}/{lat}/{lon}'
             },
-            'osmfr-breton': {
-                'language_code': 'br'
-            },
-            'osmfr-occitan': {
-                'language_code': 'oc'
+            'mapaszlakow-incline': {
+                'nameShort': 'Incline PL',
+                'mapUrl': 'http://mapaszlakow.eu/#{zoom}/{lat}/{lon}'
             }
         };
 
