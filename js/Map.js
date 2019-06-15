708b35f6426f36b352fc2ae4258079e9834c4c97
Norbert Renner <norbert.renner@gmx.de>
2019-06-15 11:10:13 +0200

    Set default zoom to 5 instead of 6, because of longer caching (#205)

--- js/Map.js
+++ js/Map.js
@@ -19,7 +19,7 @@ BR.Map = {
             })
             .addTo(map);
         if (!map.restoreView()) {
-            map.setView([50.99, 9.86], 6);
+            map.setView([50.99, 9.86], 5);
         }
 
         // two attribution lines by adding two controls, prevents ugly wrapping on
