ba0bb39fd5075ee28f575d7063251bcb5de8d258
Henrik Fehlauer <rkflx@lab12.net>
2020-06-08 18:00:00 +0000

    Add shortcut to toggle Strava layer
    Press 'S' to show the Strava buttons (if the API key has been set) as
    well as the Strava segments layer (once it has been fetched).
    
    Note that fetching and updating data from Strava still needs to be
    triggered manually by clicking the respective biking or running button.

--- js/plugin/stravaSegments.js
+++ js/plugin/stravaSegments.js
@@ -16,6 +16,12 @@ BR.stravaSegments = function(map, layersControl) {
         );
     };
 
+    L.setOptions(this, {
+        shortcut: {
+            toggleLayer: 83 // char code for 's'
+        }
+    });
+
     // hide strava buttons when layer is inactive
     var toggleStravaControl = function() {
         var stravaBar = stravaControl.runningButton.button.parentElement;
@@ -23,6 +29,21 @@ BR.stravaSegments = function(map, layersControl) {
     };
     toggleStravaControl();
     stravaControl.stravaLayer.on('add remove', toggleStravaControl);
+
+    L.DomEvent.addListener(
+        document,
+        'keydown',
+        function(e) {
+            if (BR.Util.keyboardShortcutsAllowed(e) && e.keyCode === this.options.shortcut.toggleLayer) {
+                if (map.hasLayer(stravaControl.stravaLayer)) {
+                    map.removeLayer(stravaControl.stravaLayer);
+                } else {
+                    map.addLayer(stravaControl.stravaLayer);
+                }
+            }
+        },
+        this
+    );
 
     return stravaControl;
 };
