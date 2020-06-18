8a66a816f77b33f8eb526f78915a5046e630aded
Henrik Fehlauer <rkflx@lab12.net>
2020-06-18 18:00:00 +0000

    Run `yarn lint`, `yarn prettier` and `yarn build i18next`
    0d6cd61 was still missing some style fixes, making `yarn` touch
    unrelated files on a subsequent `git commit`.

--- js/index.js
+++ js/index.js
@@ -305,7 +305,7 @@
 
         BR.tracksLoader(map, layersControl, routing);
 
-        BR.routeLoader(map, layersControl, routing,pois);
+        BR.routeLoader(map, layersControl, routing, pois);
 
         pois.addTo(map);
         routingPathQuality.addTo(map);
